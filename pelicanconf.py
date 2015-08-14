2a9ce7960dd7eca8f33c6e98d022108eecbe9768
Chris Krycho <chris@krycho.com>
2015-08-14 10:16:47 -0400

    Add content root files and configuration.

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,6 +7,7 @@ SITENAME = 'Sap.py'
 SITEURL = ''
 
 PATH = 'content'
+THEME_STATIC_DIR = ''
 
 TIMEZONE = 'America/New_York'
 
@@ -19,24 +20,35 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 10
 
+# Directories to copy without modification
+STATIC_PATHS = ['images', 'root']
+# Redirect `root` files to the root of the output.
+EXTRA_PATH_METADATA = {'root/CNAME': {'path': 'CNAME'},
+                       'root/favicon.ico': {'path': 'favicon.ico'},
+                       'root/favicon.png': {'path': 'favicon.png'},
+                       'root/robots.txt': {'path': 'robots.txt'},
+                       'root/.nojekyll': {'path': '.nojekyll'}}
+
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+# Plugin (mostly just Pandoc!) configuration
+PLUGIN_PATHS = ['../../pelican-plugins']
+PLUGINS = ['pandoc_reader']
+PANDOC_ARGS = ['--smart',  # use smart typography
+               '--no-highlight',  # use highlight.js instead
+               '-t', 'html5',  # use HTML and its corresponding attributes
+               '--section-divs',  # wrap heading-blocks with <section>
+               '--filter', 'pandoc-citeproc']
 
 # ----- Podcasting theme settings ----- #
-THEME = 'podcasting'
-HEADER_LOGO_IMG = ''  # path to header logo image
+THEME = 'podcasting-theme'
+
+# Configure the header.
+HEADER_LOGO_IMG = '/images/pythons.svg'  # path to header logo image
 HEADER_USE_LOGO_ONLY = False  # Don't use header text, just use the logo alone
-NAV_LOGO_IMG = ''  # path to a square (minimum 128⨉128px) version of the logo
+# Path from the root to a square (minimum 128⨉128px) version of the logo
+NAV_LOGO_IMG = HEADER_LOGO_IMG
 SUBTITLE = "Who says programming can't be romantic?"
